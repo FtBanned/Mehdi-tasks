c277ede5399bb289b519b13c405c94b915af6e2d
FtBanned <sirzadlimehdi48@gmail.com>
2023-02-22 20:01:58 +0400

    add

---
+++ js/Task #2/task #1/app.js
@@ -0,0 +1,3 @@
+let ad = prompt("Adinizi daxil edin")
+let soyad = prompt('Soyadinizi daxil edin')
+alert(ad+soyad)
---
+++ js/Task #2/task #3/app.js
@@ -0,0 +1,4 @@
+ let a = prompt('duzbucaqlinin enini daxil edin')
+ let b = prompt('duzbucaqlinin uzunlugunu daxil edin')
+let perimetr = Number (a*2) + Number (b*2) 
+ console.log(perimetr)
