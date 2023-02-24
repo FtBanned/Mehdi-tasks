da5c988a968bd14e79347c2bd510a611c094e8c3
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-02-24 14:04:08 +0400

    done ✅

--- js/Task #2/task #3/app.js
+++ js/Task #2/task #3/app.js
@@ -1,4 +1,5 @@
- let a = Number (prompt('duzbucaqlinin enini daxil edin'))
+// A: dogru 
+let a = Number (prompt('duzbucaqlinin enini daxil edin'))
  let b = Number (prompt('duzbucaqlinin uzunlugunu daxil edin'))
 let perimetr =  (a*2) +  (b*2) 
  console.log(perimetr)
