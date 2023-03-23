6f84fb8f436ca721f08207cddf96f012b348686d
FtBanned <sirzadlimehdi48@gmail.com>
2023-03-23 23:00:39 +0400

    add

--- js/Task #5/task #3/app.js
+++ js/Task #5/task #3/app.js
@@ -1,1 +1,7 @@
 //Parametr kimi ədədi qəbul edən və onu dördüncü dərəcə qüvvətə yülsəldərək konsolda göstərən fourth() funksiyasını yazın.
+
+
+function fourth (a=2,b=4) {
+    console.log (Math.pow(a,b))
+}
+fourth()
