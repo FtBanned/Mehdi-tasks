1ca298947624b6f162fed5a1f9413ff30e45630b
FtBanned <sirzadlimehdi48@gmail.com>
2023-03-25 15:37:16 +0400

    add

--- js/Task #6/task #1/app.js
+++ js/Task #6/task #1/app.js
@@ -1,5 +1,5 @@
 //prop2Taker adinda funksiya yaradin. Argument kimi obyekt alsin. Hemin obyektin 'prop-2' propertysinin value-nu cixarsin.
 function prop2Taker(obj) {
-    return obj['prop-2'];
+    console.log(obj['prop-2'])
   }
-console.log(prop2Taker({ test: 1, 'prop-2': 2 }))
+prop2Taker({ test: 1, 'prop-2': 2 })
