015dacb60248a06c9c39c62fac4dce29ccb39c65
FtBanned <sirzadlimehdi48@gmail.com>
2023-03-26 17:17:15 +0400

    add

---
+++ js/Task #7/task #10/app.js
@@ -0,0 +1,15 @@
+var birthDate = new Date(prompt ("Dogum ilinizi daxil edin"))
+var now = new Date()
+var age = now.getFullYear() - birthDate.getFullYear();
+if (now.getMonth() < birthDate.getMonth() || (now.getMonth() == birthDate.getMonth() && now.getDate() < birthDate.getDate())) {
+  age--;
+}
+var diff = now.getTime() - birthDate.getTime()
+var seconds = Math.floor(diff / 1000)
+var minutes = Math.floor(seconds / 60)
+var hours = Math.floor(minutes / 60)
+var days = Math.floor(hours / 24)
+var months = Math.floor(days / 30.4167)
+var years = Math.floor(months / 12)
+console.log("Yaşınız: " + age );
+console.log("Kecen vaxt: " + years + " il, " + months % 12 + " ay, " + days % 30.4167 + " gün, " + hours % 24 + " saat, " + minutes % 60 + "deqiqe," + seconds % 60 + " saniye");
--- js/Task #7/task #7/app.js
+++ js/Task #7/task #7/app.js
@@ -1,5 +1,5 @@
 var today = new Date();
-var HefteninGunleri = ['Bazar ertəsi', 'Cərşənbə axşamı', 'Cersenbe', 'Cümə axşamı', 'Cume','Senbe','Bazar'];
+var HefteninGunleri = ['Bazarertesi', 'Cərşənbəaxşamı', 'Cersenbe', 'Cüməaxşamı', 'Cume','Senbe','Bazar'];
 var HefteninGunleri = HefteninGunleri[today.getDay()]
 
-document.write("Bugun " + HefteninGunleri + " gunudur.")
+document.write("Bugun " + HefteninGunleri + "dir.")
