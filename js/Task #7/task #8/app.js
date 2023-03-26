90a301fb35c2419ca8396d4f64a2113e3572ba76
FtBanned <sirzadlimehdi48@gmail.com>
2023-03-26 17:53:05 +0400

    add

---
+++ js/Task #7/task #8/app.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const monthNames = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun",
+  "Iyul", "Avqust", "Sentaybr", "Oktaybr", "Noyabr", "Dekabr"]
+const currentMonth = monthNames[today.getMonth()]
+document.write("Bu ay " + currentMonth + " ayidi.")
