b92e90a9f96e517175aefed9143c9d0dcc9d90c5
FtBanned <sirzadlimehdi48@gmail.com>
2023-03-28 22:34:18 +0400

    add

---
+++ js/task #8/task #2/app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cevre = document.getElementById('cevre')
+
+button.onclick = () => {
+    const deyer = input.value
+    cevre.style.width = deyer + 'px'
+    cevre.style.height = deyer + 'px'
+}
