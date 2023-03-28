e9aee1690c855b173b6eb45638c5b18a80c38bdf
FtBanned <sirzadlimehdi48@gmail.com>
2023-03-28 21:15:13 +0400

    add

--- js/Task #7/task #10/app.js
+++ js/Task #7/task #10/app.js
@@ -2,7 +2,7 @@ var birthDate = new Date(prompt ("Dogum ilinizi daxil edin"))
 var now = new Date()
 var age = now.getFullYear() - birthDate.getFullYear();
 if (now.getMonth() < birthDate.getMonth() || (now.getMonth() == birthDate.getMonth() && now.getDate() < birthDate.getDate())) {
-  age--;
+  age--
 }
 var diff = now.getTime() - birthDate.getTime()
 var seconds = Math.floor(diff / 1000)
@@ -11,5 +11,5 @@ var hours = Math.floor(minutes / 60)
 var days = Math.floor(hours / 24)
 var months = Math.floor(days / 30.4167)
 var years = Math.floor(months / 12)
-console.log("Yaşınız: " + age );
-console.log("Kecen vaxt: " + years + " il, " + months % 12 + " ay, " + days % 30.4167 + " gün, " + hours % 24 + " saat, " + minutes % 60 + "deqiqe," + seconds % 60 + " saniye");
+console.log("Yaşınız: " + age )
+console.log("Kecen vaxt: " + years + " il, " + months % 12 + " ay, " + days % 30.4167 + " gün, " + hours % 24 + " saat, " + minutes % 60 + "deqiqe," + seconds % 60 + " saniye")
---
+++ js/task #8/task #1/app.js
@@ -0,0 +1,21 @@
+const circle1 = document.getElementById('circle1')
+const circle2 = document.getElementById('circle2')
+const circle3 = document.getElementById('circle3')
+const circle4 = document.getElementById('circle4')
+
+document.getElementById("circle1").onclick = () => {
+    document.body.style.backgroundColor = "gray"
+  }
+  
+  document.getElementById("circle2").onclick = () => {
+    document.body.style.backgroundColor = "red"
+  }
+  
+  document.getElementById("circle3").onclick = () => {
+    document.body.style.backgroundColor = "blue"
+  }
+  
+  document.getElementById("circle4").onclick = () => {
+    document.body.style.backgroundColor = "yellow"
+  }
+  
