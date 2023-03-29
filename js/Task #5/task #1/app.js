1f67a9007fc6e75d71c9b7329cb73dac763d95fe
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 13:59:04 +0400

    live legend 🐱‍💻

--- js/Task #5/task #1/app.js
+++ js/Task #5/task #1/app.js
@@ -1,4 +1,5 @@
 //logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxarsın.
+// A: ela
 function logGretting() {
     let ad = prompt("adinizi daxil edin")
     console.log(`${ad} is a JS developer`)
