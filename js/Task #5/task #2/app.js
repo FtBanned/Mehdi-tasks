015b8fba8895d110059faf0fb988d52d433326bd
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 13:59:14 +0400

    no task 👀

--- js/Task #5/task #2/app.js
+++ js/Task #5/task #2/app.js
@@ -1,2 +1,3 @@
 //İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
 //Funksiyanı 3 müxtəlif ad üçün çağırın.
+// A: no task 👀
