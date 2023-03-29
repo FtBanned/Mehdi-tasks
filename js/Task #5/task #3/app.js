c946bf3ec633ca5b64846711bd4acc5aaf4db09a
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 13:59:29 +0400

    done as 🐉

--- js/Task #5/task #3/app.js
+++ js/Task #5/task #3/app.js
@@ -1,6 +1,6 @@
 //Parametr kimi ədədi qəbul edən və onu dördüncü dərəcə qüvvətə yülsəldərək konsolda göstərən fourth() funksiyasını yazın.
 
-
+// A: ela
 function fourth (a=2,b=4) {
     console.log (Math.pow(a,b))
 }
