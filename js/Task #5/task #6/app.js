3396df97356fd0b6534b86c593507f9582c8623d
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 14:00:29 +0400

    best killer 🗡

--- js/Task #5/task #6/app.js
+++ js/Task #5/task #6/app.js
@@ -1,4 +1,5 @@
 //İstifadəçinin daxil etdiyi ədədin bölünəni olan bütün üçrəqəmli ədədlərin cəmini hesablayan proqram yazın
+// A: mohtesem
 let ed = prompt("Ededi daxil edin")
 let sum = 0
 for(let i = 100; i<=999; i++) {
