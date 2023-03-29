926ab7bdf0f3b14770ccafd6085156b6b60fb551
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 13:58:20 +0400

    lazyyyyy 🤓

--- js/Task #4/task #3/app.js
+++ js/Task #4/task #3/app.js
@@ -1,5 +1,6 @@
 //Istifadeciden eded alin. Eger eded hem 2ye ve hem de 5e bolunerse, console-a "bu istediyim ededdir" cixsin. 
 let ed = Number(prompt("Eded daxil edin"))
+// A: eded hem 2ye hem de 5e bolunerse. (2 shert ile yoxlamali idin, amma bele de okdur. )
 if(ed%10===0) {
     console.log("Bu istediyim ededdir")
 }
