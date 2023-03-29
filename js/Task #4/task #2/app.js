db0e0c7de9b5be68239353a743c22b41783f1293
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 13:57:50 +0400

    bug report 🐛

--- js/Task #4/task #2/app.js
+++ js/Task #4/task #2/app.js
@@ -1,4 +1,5 @@
 //Eger eded 2 reqemlidirse, console-a "eded ikireqemlidir" cixsin. 
+// A: for-a ehtiyac yoxdu. sadece if sherti ile yoxlaya bilerdin. 
 let ed = Number(prompt('Ededi daxil edin'))
 for(let i = 10;i<=99;i++) {
  if(ed===i) {
