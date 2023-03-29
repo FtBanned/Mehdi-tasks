b36211f4ecdf66b2782471db4f574f82c9840011
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-29 13:58:30 +0400

    live legend 🐱‍💻

--- js/Task #4/task #4/app.js
+++ js/Task #4/task #4/app.js
@@ -2,7 +2,7 @@
 let a = Number (prompt('ededi daxil edin') )
 let b = Number (prompt('ededi daxil edin') )
 let c = Number (prompt('ededi daxil edin') )
-
+// A: ela
 if (a+b+c>100) {
     console.log('cemleri kecdi 100u')
 }
