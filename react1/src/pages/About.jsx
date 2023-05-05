61922ed0e047bb66bfcdb8339b178e999eda4d9b
FtBanned <sirzadlimehdi48@gmail.com>
2023-05-05 14:26:45 +0400

    first react

---
+++ react1/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageContainer from '../PageContainer'
+import '../css/About.css'
+
+const About = () => {
+  return (
+    <PageContainer>
+      <div className='container2'></div>
+      <h1 className='about'>jshdkjjhdkjksjdlfsdjk <br />sjkjdhkasjdhksj <br />asjhdkjashkdjahsdjs</h1>
+    </PageContainer>
+  )
+}
+
+export default About
