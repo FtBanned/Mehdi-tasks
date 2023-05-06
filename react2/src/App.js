1d087cc59d41dc397419ea28b2267ffd7770f50e
FtBanned <sirzadlimehdi48@gmail.com>
2023-05-07 03:52:24 +0400

    react2

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Route, Routes } from "react-router"
+
 
 
 function App () {
   return (
-    <>
-    <Routes>
-      <Route/>
-    </Routes>
-    </>
+   <>
+   </>
   )
 }
 
