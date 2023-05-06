e8315b554d82d0d1fec949461c2f244e02de5b28
FtBanned <sirzadlimehdi48@gmail.com>
2023-05-07 03:49:18 +0400

    react2

--- react2/src/Navbar.jsx
+++ react2/src/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
   return (
     <nav className='nav'>
       <div>
-    
+      <a href="/">Home</a>
+     <a href="/about">About</a>
+     <a href="/contact">Contact</a>
       </div>
   </nav>
     
---
+++ react2/src/pages/AliData.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageContainer from '../PageContainer'
+
+const About = () => {
+  return (
+    <PageContainer>
+      <div className='container2'></div>
+    </PageContainer>
+  )
+}
+
+export default About
