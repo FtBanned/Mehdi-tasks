7503ce642af25ff74c899b5b4082c64908c1b892
FtBanned <sirzadlimehdi48@gmail.com>
2023-05-07 03:40:38 +0400

    react2

--- react2/src/App.js
+++ react2/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
     <Routes>
-      
+      <Route/>
     </Routes>
     </>
   )
---
+++ react2/src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./css/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <div>
+    
+      </div>
+  </nav>
+    
+  )
+}
+
+export default Navbar
